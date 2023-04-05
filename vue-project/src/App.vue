7db56bd22067474c75483ab08d7cb93dd2656510
OnisimBalan <94740094+OnisimBalan@users.noreply.github.com>
2023-04-06 00:29:39 +0300

    Vuetify initialization

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,6 +3,10 @@ import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
+<style lang="scss">
+@import 'src/main/webapp/content/scss/bootstrap-style-extensions.scss';
+</style>
+
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
---
+++ vue-project/vuetify.ts
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import { createVuetify } from 'vuetify'
+
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6'
+        }
+      }
+    }
+  }
+})
