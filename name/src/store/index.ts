8cb99c5cbac911b0ef6c894bfcebaefda40cb989
OnisimBalan <94740094+OnisimBalan@users.noreply.github.com>
2023-07-12 22:56:07 +0300

    xd

--- name/src/store/index.ts
+++ name/src/store/index.ts
@@ -7,3 +7,23 @@
 //   actions: {},
 //   mutations: {},
 // });
+
+// const path = require("path");
+// const express = require("express");
+
+// const app = express();
+
+// app.use("", express.static(path.join(__dirname, "public")));
+
+// app.get("/", (request, response) => {
+//   return response.sendFile("index.html", { root: "." });
+// });
+
+// app.get("/auth/discord", (request, response) => {
+//   return response.sendFile("dashboard.html", { root: "." });
+// });
+
+// const port = "3001";
+// app.listen(port, () =>
+//   console.log(`App listening at http://localhost:${port}`)
+// );
